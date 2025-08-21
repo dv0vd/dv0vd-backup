41d699320e1324ff2a483adf5314ddf4280f671a
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-08-21 07:07:26 +0300

    Fixed storage path

--- deployment/init.sh
+++ deployment/init.sh
@@ -8,21 +8,21 @@ apt install -y make &&
 apt install -y git && 
 
 #ssh
-cat /root/dv0vd.xyz/deployment/configs/linux/ssh.pub >> /root/.ssh/authorized_keys &&
+cat /root/dv0vd.xyz-backup/deployment/configs/linux/ssh.pub >> /root/.ssh/authorized_keys &&
 touch /etc/ssh/sshd_config.d/00-dv0vd.conf &&
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config.d/00-dv0vd.conf &&
 echo Port $SSH_PORT >> /etc/ssh/sshd_config.d/00-dv0vd.conf &&
 
 # fail2ban
 apt install fail2ban -y &&
-cat /root/dv0vd.xyz/deployment/configs/fail2ban/jail.local >> /etc/fail2ban/jail.local &&
-cat /root/dv0vd.xyz/deployment/configs/fail2ban/fail2ban.local >> /etc/fail2ban/fail2ban.local &&
+cat /root/dv0vd.xyz-backup/deployment/configs/fail2ban/jail.local >> /etc/fail2ban/jail.local &&
+cat /root/dv0vd.xyz-backup/deployment/configs/fail2ban/fail2ban.local >> /etc/fail2ban/fail2ban.local &&
 systemctl enable fail2ban &&
 systemctl start fail2ban &&
 
 # restart
 rm /etc/rc.local -f &&
-cp /root/dv0vd.xyz/deployment/configs/linux/rc.local /etc/rc.local
+cp /root/dv0vd.xyz-backup/deployment/configs/linux/rc.local /etc/rc.local
 chmod a+x /etc/rc.local &&
 
 reboot
