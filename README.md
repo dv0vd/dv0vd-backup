fa7d6e90605393565287e24ed054edc182316543
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-09-26 22:07:14 +0300

    Migrated from .xyz to .dev

--- README.md
+++ README.md
@@ -1,19 +1,9 @@
 # Dv0vD Backup server
-Backup server for dv0vd.xyz website.
+Backup server for dv0vd website.
 
 ## Getting started
 1) Update packages index: `apt update`.
 2) Install git: `apt install git`.
-3) Configure the `.env` file.
-4) Copy SSH private key to `../.ssh`.
-5) Copy Podman images to `./deployment/images`:
-- coturn_4.7.0.tar
-- dv0vd-https-proxy_1.1.0.tar
-- dv0vd-socks4_1.1.3.tar
-- dv0vd-socks5_1.1.1.tar
-- mongo_7.0.16.tar
-- nginx_1.27.3.tar
-- node_24.5.0-alpine.tar
-- postgres_15.14-alpine.tar
-- synapse_1.135.0.tar
-6) Run the initialization script `bash ./deployment/init.sh`.
+3) Clone repo: `git clone https://github.com/dv0vd/dv0vd-backup.git`.
+4) Configure the `.env` file.
+5) Run the initialization script `bash ./deployment/init.sh`.
--- deployment/init.sh
+++ deployment/init.sh
@@ -1,36 +1,52 @@
-# env
+#!/bin/bash
+set -e
+
+LOG_FILE="/var/log/init.log"
+
+log() {
+  local msg="$1"
+  local ts
+  ts=$(date +"%F %T")
+  local line="===================================================================="
+  echo -e "\n$line" | tee -a "$LOG_FILE"
+  echo "[INIT][$ts] $msg" | tee -a "$LOG_FILE"
+  echo "$line" | tee -a "$LOG_FILE"
+}
+
+log "Loading environment variables..."
 set -a &&
 source .env &&
-set + a &&
+set +a &&
 
-# timezone
+log "Setting timezone to UTC..."
 timedatectl set-timezone UTC &&
 
-#apt
+log "Updating system and installing required packages..."
 apt update &&
 apt upgrade -y &&
 apt install -y make &&
 apt install -y git && 
 
-#ssh
-cat /root/dv0vd.xyz-backup/deployment/configs/linux/ssh.pub >> /root/.ssh/authorized_keys &&
+log "Configuring SSH..."
+cat /root/dv0vd-backup/deployment/configs/linux/ssh.pub >> /root/.ssh/authorized_keys &&
 touch /etc/ssh/sshd_config.d/00-dv0vd.conf &&
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config.d/00-dv0vd.conf &&
 echo Port $SSH_PORT >> /etc/ssh/sshd_config.d/00-dv0vd.conf &&
-envsubst < ./deployment/configs/linux/ssh_env.conf > /root/.ssh/config &&
-chmod 600 /root/.ssh/config &&
+# envsubst < ./deployment/configs/linux/ssh_env.conf > /root/.ssh/config &&
+# chmod 600 /root/.ssh/config &&
 
-# fail2ban
+log "Configuring fail2ban..."
 apt install fail2ban -y &&
-cat /root/dv0vd.xyz-backup/deployment/configs/fail2ban/jail.local >> /etc/fail2ban/jail.local &&
-cat /root/dv0vd.xyz-backup/deployment/configs/fail2ban/fail2ban.local >> /etc/fail2ban/fail2ban.local &&
+cat /root/dv0vd-backup/deployment/configs/fail2ban/jail.local >> /etc/fail2ban/jail.local &&
+cat /root/dv0vd-backup/deployment/configs/fail2ban/fail2ban.local >> /etc/fail2ban/fail2ban.local &&
 systemctl enable fail2ban &&
 systemctl start fail2ban &&
 
-# restart
+log "Configuring rc.local autostart..."
 rm /etc/rc.local -f &&
-cp /root/dv0vd.xyz-backup/deployment/configs/linux/rc.local /etc/rc.local
+cp /root/dv0vd-backup/deployment/configs/linux/rc.local /etc/rc.local
 chmod a+x /etc/rc.local &&
 
+log "Initialization finished. Rebooting now..."
 reboot
 
