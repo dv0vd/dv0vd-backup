a01973f50d003f53574b9e78b987a7b3d80abad2
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-09-26 22:09:08 +0300

    Added more logs command

--- make/help.mk
+++ make/help.mk
@@ -4,6 +4,8 @@ RESET='\033[0m'
 help:
 	@echo ${GREEN}logs-clear'                   '${WHITE}— clear journalctl logs olden then 1 day${RESET}
 	@echo ${GREEN}logs-auth'                    '${WHITE}— get SSH connection attemps logs${RESET}
+	@echo ${GREEN}logs-init'                    '${WHITE}— get init logs${RESET}
+	@echo ${GREEN}logs-startup'                 '${WHITE}— get startup logs${RESET}
 	@echo ${GREEN}fail2ban-status'              '${WHITE}— get fail2ban jails status${RESET}
 	@echo ${GREEN}fail2ban-unban-all'           '${WHITE}— unban all IPs in fail2ban${RESET}
 	@echo ${GREEN}on-startup'                   '${WHITE}— commands to execute immediately after server startup${RESET}
--- make/logs.mk
+++ make/logs.mk
@@ -3,3 +3,9 @@ logs-clear:
 
 logs-auth:
 	journalctl -u ssh -n 10000 -f
+
+logs-init:
+	cat /var/log/init.log
+
+logs-startup:
+	cat /var/log/on-startup.log
