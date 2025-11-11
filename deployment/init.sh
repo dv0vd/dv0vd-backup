50f495858b925f3a580052f18aec3331558189d7
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-11-11 07:54:24 +0300

    Refactored init + fixed dns leak

--- deployment/init.sh
+++ deployment/init.sh
@@ -1,52 +1,85 @@
 #!/bin/bash
-set -e
+set -e # stop script on any error
 
-LOG_FILE="/var/log/init.log"
+
+
+configure_fail2ban() {
+  log "Configuring fail2ban..."
+  cp /root/dv0vd/deployment/configs/fail2ban/jail.local /etc/fail2ban/jail.local
+  cp /root/dv0vd/deployment/configs/fail2ban/fail2ban.local /etc/fail2ban/fail2ban.local
+  systemctl enable fail2ban
+  systemctl start fail2ban
+  log "Fail2ban successfully configured"
+}
+
+configure_ssh() {
+  log "Configuring SSH..."
+  cat /root/dv0vd/deployment/configs/ssh/ssh.pub >> /root/.ssh/authorized_keys
+  touch /etc/ssh/sshd_config.d/00-dv0vd.conf
+  echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config.d/00-dv0vd.conf
+  echo Port $SSH_PORT >> /etc/ssh/sshd_config.d/00-dv0vd.conf
+  log "SSH successfully generated"
+}
+
+finish() {
+  log "Configuring rc.local autostart..."
+  rm /etc/rc.local -f
+  cp /root/dv0vd/deployment/configs/linux/rc.local /etc/rc.local
+  chmod a+x /etc/rc.local
+  log "rc.local autostart successfully configured"
+  log "Initialization finished. Rebooting now..."
+  reboot
+}
+
+install_packages() {
+  log "Updating system and installing required packages..."
+  apt update
+  apt upgrade -y
+  apt install -y make
+  apt install -y git
+  apt install -y fail2ban
+  log "Packages successfully installed"
+}
+
+load_env() {
+  log "Loading environment variables..."
+  set -a
+  source .env
+  set +a
+  log "Environment variables successfully loaded"
+}
 
 log() {
+  local log_file="/var/log/init.log"
   local msg="$1"
   local ts
   ts=$(date +"%F %T")
   local line="===================================================================="
-  echo -e "\n$line" | tee -a "$LOG_FILE"
-  echo "[INIT][$ts] $msg" | tee -a "$LOG_FILE"
-  echo "$line" | tee -a "$LOG_FILE"
+  echo -e "\n$line" | tee -a "$log_file"
+  echo "[INIT][$ts] $msg" | tee -a "$log_file"
+  echo "$line" | tee -a "$log_file"
 }
 
-log "Loading environment variables..."
-set -a &&
-source .env &&
-set +a &&
+set_timezone() {
+  log "Setting timezone to UTC..."
+  timedatectl set-timezone UTC
+  log "Timezone successfully set"
+}
 
-log "Setting timezone to UTC..."
-timedatectl set-timezone UTC &&
+configure_dns() {
+  log "Configuring DNS..."
+  touch /etc/resolv.conf || true
+  echo $DNS1 >> /etc/resolv.conf
+  echo $DNS2 >> /etc/resolv.conf
+  log "DNS successfully configured"
+}
 
-log "Updating system and installing required packages..."
-apt update &&
-apt upgrade -y &&
-apt install -y make &&
-apt install -y git && 
 
-log "Configuring SSH..."
-cat /root/dv0vd-backup/deployment/configs/linux/ssh.pub >> /root/.ssh/authorized_keys &&
-touch /etc/ssh/sshd_config.d/00-dv0vd.conf &&
-echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config.d/00-dv0vd.conf &&
-echo Port $SSH_PORT >> /etc/ssh/sshd_config.d/00-dv0vd.conf &&
-# envsubst < ./deployment/configs/linux/ssh_env.conf > /root/.ssh/config &&
-# chmod 600 /root/.ssh/config &&
 
-log "Configuring fail2ban..."
-apt install fail2ban -y &&
-cat /root/dv0vd-backup/deployment/configs/fail2ban/jail.local >> /etc/fail2ban/jail.local &&
-cat /root/dv0vd-backup/deployment/configs/fail2ban/fail2ban.local >> /etc/fail2ban/fail2ban.local &&
-systemctl enable fail2ban &&
-systemctl start fail2ban &&
-
-log "Configuring rc.local autostart..."
-rm /etc/rc.local -f &&
-cp /root/dv0vd-backup/deployment/configs/linux/rc.local /etc/rc.local
-chmod a+x /etc/rc.local &&
-
-log "Initialization finished. Rebooting now..."
-reboot
-
+load_env
+set_timezone
+install_packages
+configure_dns
+configure_ssh
+configure_fail2ban
+finish
