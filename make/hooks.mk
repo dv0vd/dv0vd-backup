880143cdfa8f7d1e83d0f07fab790e38e56ee13d
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-11-17 20:58:47 +0300

    Enabled iptables

--- make/hooks.mk
+++ make/hooks.mk
@@ -3,7 +3,7 @@ on-startup:
 	- $(MAKE) fail2ban-stop
 	- ip addr add 9.9.9.9/32 dev lo || true
 	- bash -c 'set -a; . .env; set +a; envsubst "\$$SSH_PORT" < ./deployment/configs/iptables/iptables_env.sh > ./deployment/configs/iptables/iptables.sh'
-# - chmod +x ./deployment/configs/iptables/iptables.sh && ./deployment/configs/iptables/iptables.sh
+	- chmod +x ./deployment/configs/iptables/iptables.sh && ./deployment/configs/iptables/iptables.sh
 	- shutdown -r 23:00
 	- $(MAKE) fail2ban-start
 	- echo "nameserver ${DNS1}" > /etc/resolv.conf
